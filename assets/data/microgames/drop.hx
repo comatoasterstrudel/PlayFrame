d687188b580048eb9030f053d01d93ddf3b246ab
comatoasterstrudel <123904583+comatoasterstrudel@users.noreply.github.com>
2025-05-07 21:02:45 -0500

    drop assets

---
+++ assets/data/microgames/drop.hx
@@ -0,0 +1,12 @@
+function create():Void{
+    PlayState.wonMicrogame = false;
+}
+
+function update(elapsed:Float):Void{
+}
+
+function endMicrogame():Void{
+}
+
+function destroyMicrogame():Void{ 
+}
